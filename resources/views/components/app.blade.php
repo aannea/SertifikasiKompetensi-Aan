e400ca6857b3abf56a8fdc59590c10121b2940ba
aannea <aanotresponding@gmail.com>
2024-05-30 04:48:10 +0700

    [update]img and navbar

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hotel Aryo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <nav class="container navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+    <nav class=" navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container">
             <a class="navbar-brand" href="/">Aryo's Hotel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -37,4 +40,5 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -6,7 +6,7 @@
         @foreach ($rooms as $room)
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="{{ asset('images/' . $room->type . '.jpg') }}" class="card-img-top" alt="{{ $room->type }}">
+                    <img src="{{ asset('img/' . $room->type . '.jpeg') }}" class="card-img-top" alt="{{ $room->type }}">
                     <div class="card-body">
                         <h5 class="card-title">Kamar {{ $room->type }}</h5>
                         <p class="card-text">Harga: Rp {{ $room->price }} per night</p>
